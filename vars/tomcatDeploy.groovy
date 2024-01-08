282a79dc0a70881d4a110cee0c41790ea5aa4619
ashabasavaraj <121869922+ashabasavaraj@users.noreply.github.com>
2024-01-08 09:46:58 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -3,5 +3,6 @@ def call(tomcatIp,userName,credId,warName){
     sh "scp -o StrictHostKeyChecking=no target/${warName} ${userName}@${tomcatIp}:/opt/tomcat9/webapps/"
     sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/shutdown.sh"
     sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/startup.sh"
+    echo "hi"
   }
 }
